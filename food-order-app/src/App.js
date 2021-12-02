f8a316ef8253c4ba3ad5cf52e539bdbada56f34d
Matijahh <matijaradofficial@gmail.com>
2021-12-02 19:54:38 +0100

    App Root Design Established

---
+++ food-order-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
